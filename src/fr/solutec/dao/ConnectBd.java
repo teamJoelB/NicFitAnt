981cf4f3f486c941e8dab2067e82a28db98464f8
ESIC <ESIC@192.168.1.32>
2019-05-22 15:47:16 +0200

    ajout connexion BD

--- src/fr/solutec/dao/ConnectBd.java
+++ src/fr/solutec/dao/ConnectBd.java
@@ -5,10 +5,30 @@
  */
 package fr.solutec.dao;
 
-/**
- *
- * @author Joel B
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+//Connexion base de données
+
 public class ConnectBd {
+    protected static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    protected static final String DB_NAME="mydb";
+    protected static final String URL="jdbc:mysql://localhost/" + DB_NAME;
+    protected static final String USER="root";
+    protected static final String PASSWORD="";
     
+    static{
+        try{
+            Class.forName(DRIVER_NAME).newInstance();
+        }catch (Exception e){
+            System.out.println("Le pilote ne s'est pas chargé.");
+        }
+    }
+    
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    
+    }
 }
