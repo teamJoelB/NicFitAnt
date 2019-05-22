1c71df95b7f6b9ba31b13107ff8345dbc923d4c7
MartinLenglet <martinlenglet@gmail.com>
2019-05-22 12:30:58 +0200

    Messagerie

--- src/fr/solutec/model/User.java
+++ src/fr/solutec/model/User.java
@@ -11,4 +11,15 @@ package fr.solutec.model;
  */
 public class User {
     
+   
+    
+    private int id ;
+    private String login;
+    private String nom ;
+    private String prenom ;
+    private String mail;
+    private String mdp ; 
+    private double Poid;
+    private double taille;
+    
 }
